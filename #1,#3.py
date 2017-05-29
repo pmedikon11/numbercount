66c6491dafcfea0803e50a2ed431458d2f1a814f
pmedikon11 <pmedikon11@gmail.com>
2017-05-29 14:53:18 -0500

    Create #1,#3.py

---
+++ #1,#3.py
@@ -0,0 +1,11 @@
+x=2
+#The if block is for detecting if the number is odd or even
+if x%2 != 0:
+    x=x+1
+    #This statement prints the first even number if the number is odd    
+    print(x)
+
+
+for y in range(0,5):
+    x=x+2
+    print(x)
