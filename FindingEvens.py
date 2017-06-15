e0d423721d2d4a65592f120a1cfebeca8392aab4
pmedikon11 <pmedikon11@gmail.com>
2017-06-15 16:01:29 -0500

    Update FindingEvens.py

--- FindingEvens.py
+++ FindingEvens.py
@@ -1,8 +1,9 @@
 #Will find and Print all even numbers between 1000 and 3000
 x=1000
-c={}
 while x<3002:
     if x%2==0:
-        c={x}
-        print(c)
+        c=x
+        print( c , end = ",")
     x=x+1
+for y in(1000,3000):
+   print( [c ])
